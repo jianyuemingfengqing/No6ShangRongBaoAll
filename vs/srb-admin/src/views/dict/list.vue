f17aa7cfc3c3dd64c82a28d3ef3755ea293b715f
ljy <link.twilight.gu@gmail.com>
2022-11-24 22:47:17 +0800

    数据显示

--- vs/srb-admin/src/views/dict/list.vue
+++ vs/srb-admin/src/views/dict/list.vue
@@ -27,8 +27,12 @@
       :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
       <el-table-column
         prop="name"
-        label="姓名"
+        label="名称"
         width="180">
+      </el-table-column>
+      <el-table-column
+        prop="dictCode"
+        label="编码">
       </el-table-column>
       <el-table-column
         prop="value"
@@ -77,21 +81,11 @@
         })
       },
       load(tree, treeNode, resolve) {
-        setTimeout(() => {
-          resolve([
-            {
-              id: 31,
-              date: '2016-05-01',
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-              id: 32,
-              date: '2016-05-01',
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄'
-            }
-          ])
-        }, 1000)
+        dict.getDictByPid(tree.id).then(response => {
+          resolve(
+            response.data.items
+          )
+        })
       }
     }
   }
